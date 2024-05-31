faf113d5ca8480685a7cde009f578b8242093171
chllamas <chrisllamas2000@gmail.com>
2024-05-31 09:17:18 -0700

    restarting

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var dropdown = document.getElementById("dropdown-links");
-    if(dropdown.style.display === "grid"){
-        dropdown.style.display = "none";
-    } else {
-        dropdown.style.display = "grid";
-    }
-}
